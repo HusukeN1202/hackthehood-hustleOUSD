fb5a687a1f6c2146b8f4a160a81390587e26a366
HusukeN1202 <husukenishioka@gmail.com>
2025-07-11 16:40:02 -0700

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,25 +1,25 @@
-#checking = "yes"
+checking = "yes"
 
-#while checking == "yes":
-    #print("What is your age: ")
-    #user_input = int(input())
-    #if user_input >= 18:
-        #print("You can vote")
-    #else:
-        #print("You cant vote")
-    #print("Would you like to check another age?")
-    #user_input2 = input()
-    #checking = user_input2
+while checking == "yes":
+    print("What is your age: ")
+    user_input = int(input())
+    if user_input >= 18:
+        print("You can vote")
+    else:
+        print("You cant vote")
+    print("Would you like to check another age?")
+    user_input2 = input()
+    checking = user_input2
     
-#list1 = [3, -1, 0, 6, -4]
+list1 = [3, -1, 0, 6, -4]
 
-#for x in list1:
-    #if x > 0:
-        #print("Value is positive")
-    #elif x < 0:
-        #print("Value is negative")
-    #else:
-        #print("Number is  0")
+for x in list1:
+    if x > 0:
+        print("Value is positive")
+    elif x < 0:
+        print("Value is negative")
+    else:
+        print("Number is  0")
 
 inventory =  ["tnt", "glass", "grass", "Netherite", "Waxed Lightly Weathered Chiseled Copper Stairs"]
 
